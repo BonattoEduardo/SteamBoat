7aa3f79e6feb2f5979c22a73ce3e3055e9523f19
andrelquiosi <andre.quiosi@Outlook.com>
2022-11-17 21:06:10 -0300

    mais teste

---
+++ src/register.js
@@ -0,0 +1,27 @@
+import { REST, Routes } from 'discord.js';
+import "dotenv/config";
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'teste',
+    description: 'carlos'
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
+}
+})();
